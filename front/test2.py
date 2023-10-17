a989d03d428c187f9a36b01185ec1502b4f7e4a5
Gustavo Nunes <gustavompn06@gmail.com>
2023-10-17 13:05:19 -0300

    Limpeza de arquivos

--- front/test2.py
+++
@@ -1,6 +0,0 @@
-import json
-import datetime
-
-
-banana = f"""{"{"}"nome": "{form.nome.data}", "data_nascimento": "{form.data_nascimento.data}", "numero": "{form.numero.data}", "endereco": "{form.endereco.data}", "cargo": "{form.cargo.data}"{"}"}"""
-print(banana)
